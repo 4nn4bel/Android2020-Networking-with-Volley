c5c17702cb0cb25627ba9ec1242d2ef8eda17dad
Meisam <Meisam@meiCode.org>
2020-06-02 13:11:23 +0400

    The source code for the video called 'Networking with Volley' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/volleyexample/MainActivity.java
@@ -0,0 +1,80 @@
+package org.meicode.volleyexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+    }
+
+    private void getData() {
+
+        final Gson gson = new Gson();
+
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+//                textView.setText(response);
+//                Post[] posts = gson.fromJson(response, Post[].class);
+//                for (Post p: posts) {
+//                    textView.setText(textView.getText() + "\n" +  p.getTitle());
+//                }
+                Post post = gson.fromJson(response, Post.class);
+                textView.setText(post.getTitle());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> post = new HashMap<>();
+                post.put("id", "6");
+                post.put("userId", "102");
+                post.put("title", "New Post");
+                post.put("body", "the body of the new post");
+                return post;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+        queue.start();
+    }
+}
